f3aac53a2c36da619ad0f9d70047547a41ed5176
Ricardo Gerardi <ricardo.gerardi@gmail.com>
2026-01-18 23:06:04 -0500

    Initial commit

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,9 +8,14 @@ set -ouex pipefail
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+dnf5 remove -y firefox.x86_64 firefox-langpacks.x86_64
+
+dnf5 remove -y sddm sddm-* kde-settings* plasma-*
+
+dnf5 autoremove -y
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+#dnf5 install -y tmux
 
 # Use a COPR Example:
 #
@@ -19,6 +24,61 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+dnf5 -y install SDL2_image \
+		SDL3_image \
+		SDL3_ttf \
+		borgbackup \
+		breeze-cursor-theme \
+		breeze-gtk-common \
+		breeze-gtk-gtk3 \
+		breeze-gtk-gtk4 \
+		breeze-icon-theme \
+		cascadia-fonts-all \
+		chafa \
+		curl \
+		dunst \
+		f43-backgrounds-base \
+		fastfetch \
+		fontawesome-fonts-all \
+		foot \
+		foot-terminfo \
+		fprintd \
+		fprintd-pam \
+		gdisk \
+		glow \
+		google-noto-sans-fonts \
+		google-noto-sans-mono-fonts \
+		google-noto-serif-fonts \
+		grim \
+		guestfs-tools \
+		gum \
+		imv \
+		iwd \
+		jetbrains-mono-fonts-all \
+		lm_sensors \
+		niri \
+		nwg-bar \
+		parted \
+		podman-compose \
+		podman-machine \
+		podman-tui \
+		powertop \
+		rclone \
+		socat \
+		slurp \
+		swaybg \
+		swayidle \
+		vulkan-tools \
+		waybar \
+		wev \
+		wl-clipboard \
+		wofi \
+		zsh \
+		zsh-autosuggestions \
+		zsh-syntax-highlighting
+
+dnf5 clean all
+
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
